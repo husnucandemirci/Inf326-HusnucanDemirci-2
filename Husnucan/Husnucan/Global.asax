789ffc5d523b9f5ba1112e2848701ec3dffaf5f8
Hüsnücan Demirci <hsn__1@hotmail.com>
2015-11-04 09:20:51 +0200

    Projenin Son Hali
    Son hali

---
+++ Husnucan/Husnucan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Husnucan.MvcApplication" Language="C#" %>
---
+++ Husnucan/Husnucan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Husnucan.Startup))]
+namespace Husnucan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
